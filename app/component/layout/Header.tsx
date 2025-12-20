1cb0bf84794c80cce0fedf9f482514fd250b99b2
AliAsgar8 <aliasgaransari825@gmail.com>
2025-12-20 20:45:37 +0530

    feat: add header & motion dependency

---
+++ app/component/layout/Header.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center mt-5 px-5 py-2 font-serif">
+      <div>
+        {/* <Image src="/images/logo.png" width={100} height={25} alt="logo" /> */}
+        <h1 className="text-4xl">
+          Ali<span className="text-pink-700">.</span>
+        </h1>
+      </div>
+      <div className="flex gap-x-5">
+        <Link href="/">About</Link>
+        <Link href="/">Work</Link>
+        <Link href="/">Contact</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
